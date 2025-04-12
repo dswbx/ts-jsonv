f09ef4215a577b406ff51f87b014d06e998e8599
dswbx <dennis.senn@gmx.ch>
2025-04-12 16:20:36 +0200

    init validator tests

---
+++ src/test/validation.test.ts
@@ -0,0 +1,64 @@
+import { Validator } from "@cfworker/json-schema";
+import * as s from "../lib";
+import { describe, expect, test } from "bun:test";
+import Ajv from "ajv";
+
+const schemas = [
+   {
+      name: "simple object",
+      schema: s.object({
+         name: s.string(),
+      }),
+      data: [
+         [{ name: "John" }, true],
+         [{ name: 1 }, false],
+      ],
+   },
+   {
+      name: "object with optional",
+      schema: s.object({
+         name: s.optional(s.string()),
+         age: s.number({ minimum: 18 }),
+      }),
+      data: [
+         [{ name: "John", age: 20 }, true],
+         [{ name: "John", age: 17 }, false],
+         [{ age: 18 }, true],
+         [{}, false],
+      ],
+   },
+] as const;
+
+const validators: {
+   name: string;
+   validate: (schema: s.TSchema, data: any) => boolean;
+}[] = [
+   {
+      name: "@cfworker/json-schema",
+      validate: (schema: s.TSchema, data: any) => {
+         return new Validator(schema).validate(data).valid;
+      },
+   },
+   {
+      name: "ajv",
+      validate: (schema: s.TSchema, data: any) => {
+         const ajv = new Ajv();
+         return ajv.validate(schema, data);
+      },
+   },
+] as const;
+
+describe("validation", () => {
+   for (const { name, validate } of validators) {
+      describe(name, () => {
+         for (const { name, schema, data } of schemas) {
+            test(name, () => {
+               for (const [_data, valid] of data) {
+                  const result = validate(schema, _data);
+                  expect(result).toBe(valid);
+               }
+            });
+         }
+      });
+   }
+});
